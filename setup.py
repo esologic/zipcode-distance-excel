174829e6b3fef12b03188e27fb00efd300f11148
Devon Bray <dev@esologic.com>
2017-10-06 16:26:08 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='sample',
+    name='zipcode-distance-excel',
 
     version='1.0.0',
 
